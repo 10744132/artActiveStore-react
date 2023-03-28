dd015dd3cf5c91d764121c90d7c07c139484fd1e
10744132 <zx555589@gmail.com>
2023-03-28 22:03:10 +0800

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/artActiveStore-react/',
-  plugins: [react()]
-
+  plugins: [react()],
+  base:'/artActiveStore-react'
 })
